9910e3d1d94b547aca8ec4932f566c7f9abd1241
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-26 13:57:52 +0530

    basic setup

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const http = require("http");
 const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+const exp = require("constants");
+
+const app = express();
