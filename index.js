93d8dff03abfb76fa7562a3c98ce81cb92ef44d6
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-26 14:09:44 +0530

    setting ports

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const socketIO = require("socket.io");
 const exp = require("constants");
 
 const app = express();
+const port = 4500 || process.env.PORT; // if hosting online, selects the given port
+
+// creating a server
+const server = http.createServer(app);
+
+// creating circuit | connection
+const io = socketIO(server);
+
+// establishing port
+server.listen(port, ()=> {
+    console.log(`server is working on ${port}`);
+})
