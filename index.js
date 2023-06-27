ccafe9ab82a959ec45c985b346af8d7dc4761f5f
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-28 00:09:42 +0530

    LEVEL-3

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const exp = require("constants");
 const app = express();
 const port = 4500 || process.env.PORT; // if hosting online, selects the given port
 
+// user array
+const users = [{}];
+
 // cors : used for inter communication btw URL
 app.use(cors());
 app.get("/" , (req,res)=> {
@@ -24,8 +27,20 @@ io.on("connection" , (socket)=>{
     console.log("New Connnection");
 
     socket.on('joined', ({user}) => {
+        users[socket.id] = user;
         console.log(`${user} has joined`);
-    })
+
+        // on join message
+        socket.emit('welcome' , {user:"Admin", message:"Welcome to the chat!"});
+
+        // to all except joined user
+        socket.broadcast.emit('userJoined',  {user:"Admin", message: `${users[socket.id]} has joined`});
+    });
+
+    socket.on('userDisconnect', () => {
+        console.log(`${users[socket.io]} left`);
+        socket.broadcast.emit('userLeft',  {user:"Admin", message: `${users[socket.id]} left`});
+    });
 })
 
 // establishing port
